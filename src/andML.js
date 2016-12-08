4af4b765636125f131320c0453756cd7d63bc211
andfisher <and.fisher@hotmail.com>
2016-12-08 22:28:14 +0000

    Hello files
    First commit to repo

---
+++ src/andML.js
@@ -0,0 +1,252 @@
+/**
+ * @desc andML. A JavaScript Object to HTML parser
+ * @author Andrew Fisher
+ * @copyright Copyright (c) 2016 Andrew Fisher (andfisher)
+ * @version 0.0.1
+ * @license The MIT License (MIT)
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+ 
+;(function(undefined){
+	'use strict';
+
+	window.andML = (function() {
+	
+		var _version = '0.1.0';
+		
+		var _settings = {
+			supportSVG: false,
+			supportXML: false
+		};
+		
+		var _tags = 'a,abbr,acronym,address,applet,area,article,aside,audio,b,base,basefont,bdi,bdo,bgsound,big,blink,blockquote,body,br,button,canvas,caption,center,cite,code,col,colgroup,command,content,data,datalist,dd,del,details,dfn,dialog,dir,div,dl,dt,element,em,embed,fieldset,figcaption,figure,font,footer,form,frame,frameset,h1,h2,h3,h4,h5,h,head,header,hgroup,hr,html,i,iframe,image,img,input,ins,isindex,kbd,keygen,label,legend,li,link,listing,main,map,mark,marquee,menu,menuitem,meta,meter,multicol,nav,nobr,noembed,noframes,noscript,object,ol,optgroup,option,output,p,param,picture,plaintext,pre,progress,q,rp,rt,rtc,ruby,s,samp,script,section,select,shadow,small,source,spacer,span,strike,strong,style,sub,summary,sup,table,tbody,td,template,textarea,tfoot,th,thead,time,title,tr,track,tt,u,ul,var,video,wbr,xmp'.split(',');
+		
+		var _svg = '?xml,circle,ellipse,line,path,polygon,polyline,rect,svg'.split(',');
+		
+		var _voids = 'area,base,br,col,command,embed,hr,img,input,keygen,link,meta,param,source,track,wbr'.split(',');
+
+		var _tagNS = function(){
+			return (_settings.supportXML ? '=node' : 'tag');
+		}
+		var _textNS = function(){
+			return (_settings.supportXML ? '=text' : 'text');
+		}
+		var _childrenNS = function() {
+			return (_settings.supportXML ? '=nodes' : 'children');
+		}
+		
+		var _emptyObj = function(o) {
+			return JSON.stringify(o) === '{}';
+		}
+		
+		var _isArray = function(o) {
+			if (Array.isArray) return Array.isArray(o);
+			return Object.prototype.toString.call(o) === '[object Array]';
+		}
+		
+		var _extend = function() {
+		
+			var _array = [], len = arguments.length;
+			while(len--) {
+				if (typeof arguments[len] === 'object') {
+					for (var i in arguments[len]) {
+						if (arguments[len].hasOwnProperty(i)) {
+							_array.push(arguments[len][i]);
+						}
+					}
+				}
+			}
+			return _array;
+		}
+		
+		var _parse = function(o) {
+
+			var _, m;
+			
+			function _merge() {
+				var _merged = {}, len = arguments.length;
+				while(len--) {
+					if (typeof arguments[len] === 'object') {
+						for (var i in arguments[len]) {
+							if (arguments[len].hasOwnProperty(i)) {
+								_merged[i.toLowerCase()] = arguments[len][i];
+							}
+						}
+					}
+				}
+				return _merged;
+			}
+		
+			function _keys(str) {
+			
+				var _parsed = {}, _tag, m;
+
+				if(!_settings.supportXML && _tags.indexOf(str) >= 0) {
+					_parsed[_tagNS()] = str;
+					return _parsed;
+				}
+				
+				if (_settings.supportXML) {
+					m = str.match(/^[a-z0-9]*:?([a-z1-6]*)/gi);
+				} else {
+					m = str.match(/^([a-z1-6]*)/gi);
+				}
+				
+				if (m.length) {
+					_tag = m[0].toLowerCase();
+					if (!_settings.supportXML && _tags.indexOf(_tag) < 0) {
+						return false;
+					}
+					if (_tag === '') {
+						return false;
+					}
+					_parsed[_tagNS()] = _tag;
+					
+					
+					/**
+ 					 * Test for CSS selectors
+					 */
+
+					// ID
+					m = str.match(/#([^#\s\[\]\.]+)/i);
+					if (m && m.length === 2) {
+						_parsed['@id'] = m[1];
+					}
+					
+					// Attributes
+					var aRE = /\[([^\=\[\]]{1,})=?["']?([^\=\'\"]{0,})["']?\]/ig;
+					while ((m = aRE.exec(str)) !== null) {
+						_parsed['@' + m[1]] = m[2];
+					}
+					
+					// Attributes
+					var cRE = /\.([a-z0-9\-\_]{1,})/ig;
+					var cssClasses = [];
+					while ((m = cRE.exec(str)) !== null) {
+						cssClasses.push(m[1]);
+					}
+					if (cssClasses.length) {
+						_parsed['@class'] = cssClasses.join(' ');
+					}
+
+					return _parsed;
+				}
+				
+				return false;
+			}
+		
+			function _attr(obj) {
+				var attr = '';
+				for (var i in obj) {
+					if (! _settings.supportXML && i.toLowerCase() === '@data' && typeof obj[i] !== 'string') {
+					
+						for (var d in obj[i]) {
+							attr += ' data-' + d.toLowerCase() + '="' + obj[i][d] + '"';
+						}
+					
+					} else if (i.charAt(0) === '@') {
+						attr += ' ' + i.toLowerCase().substring(1) + '="' + obj[i] + '"';
+					}
+				}
+				return attr;
+			}
+		
+			for (var i in o) {
+				_ = _keys(i);
+
+				if (!_) continue;
+				
+				// @TODO
+				// Is this condition still needed?
+				if (false && typeof o[i] !== 'string' && o[i].length) {
+					for (var n in o[i]) {
+						_parse.call(this, o[i][n]);
+					}
+				} else {
+
+					_ = _merge(_, o[i]);
+
+					if (_[_tagNS()] === 'img' && _['@alt'] === undefined) {
+						_['@alt'] = '';
+					}
+
+					this._ml += '<' + _[_tagNS()] + _attr(_) + (_[_tagNS()].charAt(0) === '?' ? '?' : '') + '>';
+
+					if (_emptyObj(o[i])) {
+						// No output required.
+						
+					} else if (typeof o[i] !== 'string' && o[i].length) {
+						for (var n in o[i]) {
+							_parse.call(this, o[i][n]);
+						}						
+					} else if (typeof o[i] === 'string') {
+						
+						this._ml += o[i];
+						
+					} else {
+
+						if (_[_textNS()]) {
+							this._ml += _[_textNS()];
+						}
+
+						_parse.call(this, _);
+					
+					}
+					
+					if (_isArray(_[_childrenNS()])) {
+						for (var n in _[_childrenNS()]) {
+							_parse.call(this, _[_childrenNS()][n]);
+						}
+					}
+					
+					if (_voids.indexOf(_[_tagNS()]) < 0 && _[_tagNS()].charAt(0) !== '?') {
+						this._ml += '</' + _[_tagNS()] + '>';
+					}
+				}
+			}
+		}
+		
+		var _reverse = function(ml) {
+			// @todo
+		}
+	
+		function andML() {
+			this._version = _version;
+			this._ml = '';
+			this._on = {};
+		}
+		
+		andML.prototype.on = function(ml) {
+			this._on = {};
+			_reverse.call(this, ml);
+			return this._on;
+		};
+		andML.prototype.ml = function(on) {		
+			if (this.settings.supportSVG) {
+				_tags = _extend(_svg);
+			};
+			this._ml = '';
+			_parse.call(this, on);
+			return this._ml;
+		};
+		andML.prototype.version = _version;
+		andML.prototype.settings = _settings;
+		
+		return new andML();
+	})();
+	
+})();
---
+++ tests/spec/AndMLSpec.js
@@ -0,0 +1,574 @@
+describe('AndML', function() {
+
+  beforeEach(function() {
+    andML.settings.supportSVG = false;
+	andML.settings.supportXML = false;
+  });
+  
+  it('should have a ml() method', function() {
+	expect(typeof andML.ml).not.toEqual('undefined');
+  });
+
+  it('should have a on() method', function() {
+	expect(typeof andML.on).not.toEqual('undefined');
+  });
+  
+  describe('AndML.ml()', function() {
+  
+    beforeEach(function() {
+		andML.settings.supportSVG = false;
+		andML.settings.supportXML = false;
+	  });
+
+	  it('should parse AMLON into correct markup', function() {
+		var on = {
+			'article': {
+				'h1': 'Hello World',
+				'p': 'Lorem Ipsum dolor sit amet'
+			}
+		  };
+		var ml = '<article><h1>Hello World</h1><p>Lorem Ipsum dolor sit amet</p></article>'
+		expect(andML.ml(on)).toEqual(ml);  
+	  });
+
+	  it('should parse arrays from AMLON into correct markup', function() {
+		var on = {
+			'html': {
+				'body': [
+					{'p': 'Lorem Ipsum dolor sit amet'},
+					{'p': 'Lorem Ipsum dolor sit amet'},
+					{'p': 'Lorem Ipsum dolor sit amet'}
+				]
+			}
+		  };
+		var ml = '<html><body><p>Lorem Ipsum dolor sit amet</p><p>Lorem Ipsum dolor sit amet</p><p>Lorem Ipsum dolor sit amet</p></body></html>'
+		expect(andML.ml(on)).toEqual(ml);  
+	  });
+	  
+	  it('should allow for array-like attributes', function() {
+		var on = {
+			'input[name="variables[0][222]"]': true
+		  };
+		var ml = '<input name="variables[0][222]">';
+		expect(andML.ml(on)).toEqual(ml);  
+	  });
+	  
+	  it('should parse a full complex form', function() {
+		var on = {
+			'form[enctype="multipart/form-data"]': {
+				'@method': 'post',
+				'@action': '/submit',
+				'fieldset': [
+					{'legend': 'Enter your details'},
+					{'label[for="email"]': 'Email address'},
+					{'input#email[type="email"][name="email"]': true},
+					{'input#reset[type="reset"][value="Reset me"]': true},
+					{'button#submit[type="submit"]': 'Submit me!'}
+				]
+			}
+		  };
+		var ml = '<form method="post" action="/submit" enctype="multipart/form-data">' +
+			'<fieldset>' +
+				'<legend>Enter your details</legend>' +
+				'<label for="email">Email address</label>' +
+				'<input id="email" type="email" name="email">' +
+				'<input id="reset" type="reset" value="Reset me">' +
+				'<button id="submit" type="submit">Submit me!</button>' +
+			'</fieldset></form>'
+		expect(andML.ml(on)).toEqual(ml);  
+	  });
+	  
+	  it('should support a children node', function() {
+		var on = {
+			'div': {
+				'children': [
+					{'p': 'One'},
+					{'p': 'Two'}
+				]
+			}
+		};
+		var ml = '<div><p>One</p><p>Two</p></div>'
+		expect(andML.ml(on)).toEqual(ml);  
+	  });
+	  
+	  it('should ignore non html5 tags', function() {
+		var on = {
+			'article': {
+				'andml': 'Hello World',
+				'p': 'Lorem Ipsum dolor sit amet'
+			}
+		  };
+		var ml = '<article><p>Lorem Ipsum dolor sit amet</p></article>'
+		expect(andML.ml(on)).toEqual(ml);
+	  });
+	  
+	  it('should force lowercase tagnames and attribute keys', function() {
+	  
+		var on = {
+			'ARTICLE': {
+				'@DATA': {
+					'KEY': 'value'
+				},
+				'H1': {
+					'@CLASS': 'some-css-class',
+					'TEXT': 'Hello World'
+				},
+				'P': {
+					'@ID': 'SomeId',
+					'TEXT': 'Lorem Ipsum dolor sit amet'
+				}
+			}
+		  };
+		var ml = '<article data-key="value"><h1 class="some-css-class">Hello World</h1><p id="SomeId">Lorem Ipsum dolor sit amet</p></article>'
+		expect(andML.ml(on)).toEqual(ml);
+	  
+	  });
+	  
+	  describe('AndML.on() setting @attributes', function() {
+	 
+		  it('should add an ID attribute from @id key-value pair', function() {
+			var on = {
+				'div': {
+					'@id' : 'my-id-example'
+				}
+			};
+			var ml = '<div id="my-id-example"></div>';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  
+		  
+		  it('should parse CSS selector attributes (double quotes)', function() {
+			var on = {
+				'input[name="number"][type=text]': true
+			  };
+			var ml = '<input name="number" type="text">'
+			expect(andML.ml(on)).toEqual(ml);  
+		  });
+		  it('should parse CSS selector attributes (single quotes)', function() {
+			var on = {
+				"input[name='number'][type=text]": true
+			  };
+			var ml = '<input name="number" type="text">'
+			expect(andML.ml(on)).toEqual(ml);  
+		  });
+		  it('should parse CSS selector class names', function() {
+			var on = {
+				"input.form-field": true
+			  };
+			var ml = '<input class="form-field">'
+			expect(andML.ml(on)).toEqual(ml);  
+		  });
+		  it('should parse multiple CSS selector class names', function() {
+			var on = {
+				"input.form-field.active.text-field": true
+			  };
+			var ml = '<input class="form-field active text-field">'
+			expect(andML.ml(on)).toEqual(ml);  
+		  });
+		  
+		  it('should add set A href, target, name an rel attributes from @href key-value pair', function() {
+			var on = {
+				'a': {
+					'@href' 	: '#',
+					'@target' 	: '_blank',
+					'@name'		: 'hyperlink',
+					'@rel'		: 'test'
+				}
+			};
+			var ml = '<a href="#" target="_blank" name="hyperlink" rel="test"></a>';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  
+		  it('should parse a key into an ID attribute', function(){
+			var on = {
+				'a#element-id': {
+					text : 'Key'
+				}
+			};
+			var ml = '<a id="element-id">Key</a>';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  
+		  it('should add data attributes when passed an @data key', function(){
+			var on = {
+				'div': {
+					'@data': {
+						attribute: 'test1',
+						property: 'test2'
+					}
+				}
+			};
+			var ml = '<div data-attribute="test1" data-property="test2"></div>';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should should treat a @data key as its own attribute, if the value is a string, e.g. on OBJECT', function(){
+			var on = {
+				'object': {
+					'@data': '/object.wav'
+				}
+			};
+			var ml = '<object data="/object.wav"></object>';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  
+		  it('should generate a void AREA element with attributes', function() {
+			var on = {
+				'area': {
+					'@shape': 'circle',
+					'@coords': '90,58,3',
+					'@href':'mars.html',
+					'@alt': 'Mars'
+				}
+			};
+			var ml = '<area shape="circle" coords="90,58,3" href="mars.html" alt="Mars">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should generate a void BASE element with attributes', function() {
+			var on = {
+				'base': {
+					'@href': 'https://github.com/andfisher/andML',
+					'@target': '_blank'
+				}
+			};
+			var ml = '<base href="https://github.com/andfisher/andML" target="_blank">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  
+		  it('should generate a void BR element', function() {
+			var on = {
+				'br': true
+			};
+			var ml = '<br>';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should generate a void COL element with attributes', function() {
+			var on = {
+				'col': {
+					'@span': 2
+				}
+			};
+			var ml = '<col span="2">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should generate a void COMMAND element with attributes', function() {
+			var on = {
+				'command': {
+					'@type': 'command',
+					'@label': 'Save',
+					'@icon': 'icons/save.png',
+					'@onclick': 'save()'
+				}
+			};
+			var ml = '<command type="command" label="Save" icon="icons/save.png" onclick="save()">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should generate a void EMBED element with attributes', function() {
+			var on = {
+				'embed': {
+					'@type': 'video/quicktime',
+					'@src': 'movie.mov',
+					'@width': 640,
+					'@height': 480
+				}
+			};
+			var ml = '<embed type="video/quicktime" src="movie.mov" width="640" height="480">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should generate a void HR element', function() {
+			var on = {
+				'hr': true
+			};
+			var ml = '<hr>';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should generate a void IMG element with attributes', function() {
+			var on = {
+				'img': {
+					'@src': '/some.png',
+					'@alt': 'Some alt attribute',
+					'@width': 300,
+					'@height': 200
+				}
+			};
+			var ml = '<img src="/some.png" alt="Some alt attribute" width="300" height="200">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should generate a void INPUT element with attributes', function() {
+			var on = {
+				'input': {
+					'@type': 'text',
+					'@id': 'form_field_1',
+					'@name': 'form-field',
+					'@value': '12345'
+				}
+			};
+			var ml = '<input type="text" id="form_field_1" name="form-field" value="12345">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should generate a void KEYGEN element with attributes', function() {
+			var on = {
+				'keygen': {
+					'@name': 'name',
+					'@challenge': 'challenge string',
+					'@keytype': 'type',
+					'@keyparams': 'params'
+				}
+			};
+			var ml = '<keygen name="name" challenge="challenge string" keytype="type" keyparams="params">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should generate a void LINK element with attributes', function() {
+			var on = {
+				'link': {
+					'@type': 'text/css',
+					'@rel': 'stylesheet',
+					'@href': '/some.css'
+				}
+			};
+			var ml = '<link type="text/css" rel="stylesheet" href="/some.css">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  
+		  it('should generate a void META element with attributes', function() {
+			var on = {
+				'meta': {
+					'@property': 'og:title',
+					'@content': 'Site description'
+				}
+			};
+			var ml = '<meta property="og:title" content="Site description">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should generate a void PARAM element with attributes', function() {
+			var on = {
+				'param': {
+					'@name': 'autoplay',
+					'@value': 'true'
+				}
+			};
+			var ml = '<param name="autoplay" value="true">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should generate a void SOURCE element with attributes', function() {
+			var on = {
+				'source': {
+					'@src': 'audio.ogg',
+					'@type': 'audio/ogg'
+				}
+			};
+			var ml = '<source src="audio.ogg" type="audio/ogg">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should generate a void TRACK element with attributes', function() {
+			var on = {
+				'track': {
+					'@kind': 'subtitles',
+					'@src': 'subtitles_en.vtt',
+					'@srclang': 'en'
+				}
+			};
+			var ml = '<track kind="subtitles" src="subtitles_en.vtt" srclang="en">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+
+		  it('should generate a void WBR element', function() {
+			var on = {
+				'wbr': true
+			};
+			var ml = '<wbr>';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+	  
+	  
+	  
+		  it('should, for accessibilty, add an empty alt attribute to an IMG element if not set explicitly', function() {
+			var on = {
+				'img': {
+					'@src': '/some.png',
+					'@height': 200,
+					'@width': 400
+				}
+			};
+			var ml = '<img src="/some.png" height="200" width="400" alt="">';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+	  });
+	
+	  describe('Other formats', function() {
+  
+		  it('should not allow extended SVG elements if unconfigured', function() {
+
+			andML.settings.supportSVG = false;
+			
+			var on = {
+				'svg': {}
+			};
+
+			var ml = '';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should extend the list of allowed tags when SVG support is configured', function() {
+
+			andML.settings.supportSVG = true;
+			
+			var on = {
+				'svg': {}
+			};
+
+			var ml = '<svg></svg>';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should support ?xml declatations', function() {
+
+			andML.settings.supportSVG = true;
+			
+			var on = {
+				'?xml': {
+					'@version': '1.0',
+					'@standalone': 'no'
+				}
+			};
+
+			var ml = '<?xml version="1.0" standalone="no"?>';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should allow any node names when XML support is configured', function() {
+
+			andML.settings.supportXML = true;
+			andML.settings.debug = true;
+			var on = {
+				'xml': {
+					'library': {
+						'books': {
+							'book': {
+								'@author': 'David Wong', 
+								'=text': 'John Dies at the End'
+							}
+						}
+					}
+				}
+			};
+
+			var ml = '<xml><library><books><book author="David Wong">John Dies at the End</book></books></library></xml>';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  it('should support XML namespaces support is configured', function() {
+
+			andML.settings.supportXML = true;
+			andML.settings.debug = true;
+			var on = {
+				'xml': {
+					'@xmlns:l' : 'http://www.andfisher.com/library',
+					'@xmlns:b' : 'http://www.andfisher.com/book',
+					'l:library': {
+						'bb:books': {
+							'@xmlns:bb': 'http://www.andfisher.com/books',
+							'b:book': {
+								'@author': 'David Wong', 
+								'=text': 'John Dies at the End'
+							}
+						}
+					}
+				}
+			};
+
+			var ml = '<xml xmlns:l="http://www.andfisher.com/library" xmlns:b="http://www.andfisher.com/book"><l:library><bb:books xmlns:bb="http://www.andfisher.com/books"><b:book author="David Wong">John Dies at the End</b:book></bb:books></l:library></xml>';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  
+		  
+		  it('should allow an attribute called `data` instead of HTML `data-` attributes when using XML', function() {
+
+			andML.settings.supportXML = true;
+			andML.settings.debug = true;
+			var on = {
+				'xml': {
+					'book': {
+						'@data' : 'Some value',
+						'=text' : 'Some node string'
+					},
+					'record': {
+						'@data' : {
+							a: 'foo',
+							b: 'bar'
+						}
+					}
+				}
+			};
+
+			var ml = '<xml><book data="Some value">Some node string</book><record data="[object Object]"></record></xml>';
+			expect(andML.ml(on)).toEqual(ml);
+		  });
+		  
+	});
+  
+  });
+  
+  xdescribe('AndML.on()', function() {
+  
+	it('should parse correct markup into AMLON', function() {
+	
+		var ml = '<article><h1>Hello World</h1><p>Lorem Ipsum dolor sit amet</p></article>';
+		var on = {
+			'article': {
+				'h1': 'Hello World',
+				'p': 'Lorem Ipsum dolor sit amet'
+			}
+		 };
+		expect(andML.on(ml)).toEqual(on); 
+	
+	});
+
+  });
+ 
+  /*
+  it("should be able to play a Song", function() {
+    player.play(song);
+    expect(player.currentlyPlayingSong).toEqual(song);
+
+    //demonstrates use of custom matcher
+    expect(player).toBePlaying(song);
+  });
+
+  describe("when song has been paused", function() {
+    beforeEach(function() {
+      player.play(song);
+      player.pause();
+    });
+
+    it("should indicate that the song is currently paused", function() {
+      expect(player.isPlaying).toBeFalsy();
+
+      // demonstrates use of 'not' with a custom matcher
+      expect(player).not.toBePlaying(song);
+    });
+
+    it("should be possible to resume", function() {
+      player.resume();
+      expect(player.isPlaying).toBeTruthy();
+      expect(player.currentlyPlayingSong).toEqual(song);
+    });
+  });
+
+  // demonstrates use of spies to intercept and test method calls
+  it("tells the current song if the user has made it a favorite", function() {
+    spyOn(song, 'persistFavoriteStatus');
+
+    player.play(song);
+    player.makeFavorite();
+
+    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+  });
+
+  //demonstrates use of expected exceptions
+  describe("#resume", function() {
+    it("should throw an exception if song is already playing", function() {
+      player.play(song);
+
+      expect(function() {
+        player.resume();
+      }).toThrowError("song is already playing");
+    });
+  });
+  */
+});
