22d3a561c08db2e7edc3e36f914917add7cbcca0
andfisher <and.fisher@hotmail.com>
2017-01-01 21:34:19 +0000

    Updated SVG support for image

--- src/andML.js
+++ src/andML.js
@@ -2,7 +2,7 @@
  * @desc andML. A JavaScript Object to HTML parser
  * @author Andrew Fisher
  * @copyright Copyright (c) 2016 Andrew Fisher (andfisher)
- * @version 0.1.3
+ * @version 0.1.4
  * @license The MIT License (MIT)
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,7 @@
 
 	window.andML = (function() {
 	
-		var _version = '0.1.3';
+		var _version = '0.1.4';
 		
 		var _settings = {
 			supportSVG: false,
@@ -35,7 +35,7 @@
 		
 		var _tags = 'a,abbr,acronym,address,applet,area,article,aside,audio,b,base,basefont,bdi,bdo,bgsound,big,blink,blockquote,body,br,button,canvas,caption,center,cite,code,col,colgroup,command,content,data,datalist,dd,del,details,dfn,dialog,dir,div,dl,dt,element,em,embed,fieldset,figcaption,figure,font,footer,form,frame,frameset,h1,h2,h3,h4,h5,h,head,header,hgroup,hr,html,i,iframe,image,img,input,ins,isindex,kbd,keygen,label,legend,li,link,listing,main,map,mark,marquee,menu,menuitem,meta,meter,multicol,nav,nobr,noembed,noframes,noscript,object,ol,optgroup,option,output,p,param,picture,plaintext,pre,progress,q,rp,rt,rtc,ruby,s,samp,script,section,select,shadow,small,source,spacer,span,strike,strong,style,sub,summary,sup,table,tbody,td,template,textarea,tfoot,th,thead,time,title,tr,track,tt,u,ul,var,video,wbr,xmp'.split(',');
 		
-		var _svg = '?xml,circle,ellipse,line,path,polygon,polyline,rect,svg'.split(',');
+		var _svg = '?xml,circle,ellipse,image,line,path,polygon,polyline,rect,svg'.split(',');
 		
 		var _voids = 'area,base,br,col,command,embed,hr,img,input,keygen,link,meta,param,source,track,wbr'.split(',');
 
